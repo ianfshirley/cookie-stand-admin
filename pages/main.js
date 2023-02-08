5e1a282a8cc247cfef59383bf26f1cbee61bb264
ianfshirley <ianfshirley88@gmail.com>
2023-02-07 20:36:58 -0800

    quick fix

--- pages/main.js
+++ pages/main.js
@@ -8,16 +8,22 @@ export default function Main() {
         <form className='w-3/4 p-2 mx-auto my-4 bg-green-300 rounded'>
           <div id='location_div' className='flex justify-evenly p-3 my-4' >
             <label for='location'>Location</label>
-            <input name='location' id='location' placeholder='Location' className='flex-auto ml-2 pl-1 border border-gray-700' />
+            <input name='location' id='location' placeholder='Barcelona' className='flex-auto ml-2 pl-1 border border-gray-700' />
           </div>
           <div className='flex justify-evenly'>
-            <label for='minimum_customers_per_hour'>Min. Customers Per Hour</label>
-            <input name='minimum_customers_per_hour' id='minimum_customers_per_hour' placeholder='Min. Customers Per Hour' className='block p-2 m-2 border border-gray-700' />
-            <label for='maximum_customers_per_hour'>Max. Customers Per Hour</label>
-            <input name='maximum_customers_per_hour' id='maximum_customers_per_hour' placeholder='Max. Customers Per Hour' className='block p-2 m-2 border border-gray-700' />
-            <label for='average_cookies_per_sale'>Avg. Cookies Per Sale</label>
-            <input name='average_cookies_per_sale' id='average_cookies_per_sale' placeholder='Avg. Cookies Per Sale' className='block p-2 m-2 border border-gray-700' />
-            <button className='px-2 bg-green-600 text-gray-50 border border-gray-700 rounded'>Create</button>
+            <label>
+              Min. Customers Per Hour
+              <input name='minimum_customers_per_hour' placeholder='2' className='block p-2 m-2 border border-gray-700' />
+            </label>
+            <label>
+              Max. Customers Per Hour
+              <input name='maximum_customers_per_hour' placeholder='4' className='block p-2 m-2 border border-gray-700' />
+            </label>
+            <label>
+              Avg. Cookies Per Sale
+              <input name='average_cookies_per_sale' placeholder='2.5' className='block p-2 m-2 border border-gray-700' />
+            </label>
+            <button className='px-4 py-1 bg-green-600 text-gray-50 border border-gray-700 rounded'>Create</button>
           </div>
         </form>
 
@@ -26,4 +32,6 @@ export default function Main() {
   )
 }
 
+
+//  content-center
 // flex-auto p-2 m-2 border border-gray-700
