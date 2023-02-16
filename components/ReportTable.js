2308ed2a31397d3f0bc8e41a1ca21ea36797b9b7
ianfshirley <ianfshirley88@gmail.com>
2023-02-15 18:51:47 -0800

    added styling to report table, re-added conditional rendering

--- components/ReportTable.js
+++ components/ReportTable.js
@@ -1,44 +1,46 @@
-import {hours, hourlySales, locations}   from './data'
+import { hours, hourlySales, locations } from './data'
 import React from 'react'
 
 export default function ReportTable() {
   return (
-    <>
-      <table>
+  //   props.cookieStands.length > 0 ? (
+      <>
+      <table className="w-1/2 mx-auto my-4 border">
         <thead>
           <tr>
-            <th>Location</th>
+            <th className="border border-grey bg-emerald-500">Location</th>
             {hours.map((hour) => {
-              return <th key={hour.id}>{hour}</th>
+              return <th className='bg-emerald-500' key={hour.id}>{hour}</th>
             })}
-            <th>Totals</th>
+            <th className="border border-black bg-emerald-500">Totals</th>
           </tr>
         </thead>
         <tbody>
-        {locations.map((location, idx) => {
+          {locations.map((location, idx) => {
             return <tr key={idx}>
-                      <td>{location}</td>
-                      {hourlySales.map((hour, idx) => {
-                        return <td key={idx}>{hour}</td>
-                      })}
-                      <td>{hourlySales.reduce((sum, value) => sum + value, 0)}</td>
-                  </tr>
+              <td className="p-2 border border-black bg-emerald-500">{location}</td>
+              {hourlySales.map((hour, idx) => {
+                return <td className='bg-emerald-300' key={idx}>{hour}</td>
+              })}
+              <td className="p-2 border border-black bg-emerald-500">{hourlySales.reduce((sum, value) => sum + value, 0)}</td>
+            </tr>
           })}
         </tbody>
         <tfoot>
           <tr>
-            <td>Totals</td>
+            <td className="p-2 border border-black bg-emerald-500">Totals</td>
             {hours.map((hour, idx) => {
-              return <td key={idx}>{}</td>
+              return <td className='bg-emerald-300' key={idx}>{  }</td>
             })}
           </tr>
         </tfoot>
       </table>
     </>
+    // ) 
+    // :
+    // (
+    //   <h1>No Cookie Stands Created Yet</h1>
+    // )
   )
 }
 
-// props.cookieStands.length > 0 ? 
-// : (
-//   <h1>No Cookie Stands Created Yet</h1>
-// );
