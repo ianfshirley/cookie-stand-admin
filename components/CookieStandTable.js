c283313fdac2b896cf4d1ed434647a8a26abc1b4
ianfshirley <ianfshirley88@gmail.com>
2023-02-15 21:31:57 -0800

    updated cookiestandtable and added function to generate rows

--- components/CookieStandTable.js
+++ components/CookieStandTable.js
@@ -1,9 +1,18 @@
 import { hours, hourlySales, locations } from './data'
 import React from 'react'
+import CookieStandTableRow from './CookieStandTableRow';
 
-export default function CookieStandTable() {
+export default function CookieStandTable({cookieStands, deleteStand}) {
+
+  // calculate totals for totals row
+  const totalRow = cookieStands.reduce((acc, stand) => {
+    return cookieStand.hourly_sales.map((sale, i) => acc[i] + sale);
+  }, Array(hours.length).fill(0));
+
+  const dailyTotal = totalRow.reduce((sum, sale) => sum + sale, 0);
+
   return (
-  //   cookieStands.length > 0 ? (
+    cookieStands.length > 0 ? (
       <>
       <table className="w-1/2 mx-auto my-4 border">
         <thead>
@@ -16,7 +25,12 @@ export default function CookieStandTable() {
           </tr>
         </thead>
         <tbody>
-          {locations.map((location, idx) => {
+
+          {cookieStands.map(cookieStand => (
+            <CookieStandTableRow key={cookieStand.id} cookieStand={cookieStand} deleteStand={deleteStand}/>
+          ))}
+
+          {/* {locations.map((location, idx) => {
             return <tr key={idx}>
               <td className="p-2 border border-black bg-emerald-500">{location}</td>
               {hourlySales.map((hour, idx) => {
@@ -24,23 +38,25 @@ export default function CookieStandTable() {
               })}
               <td className="p-2 border border-black bg-emerald-500">{hourlySales.reduce((sum, value) => sum + value, 0)}</td>
             </tr>
-          })}
+          })} */}
+
         </tbody>
         <tfoot>
-          <tr>
-            <td className="p-2 border border-black bg-emerald-500">Totals</td>
-            {hours.map((hour, idx) => {
-              return <td className='bg-emerald-300' key={idx}>{  }</td>
+          <tr className="p-2 border border-black bg-emerald-500">
+            <tf>Totals</tf>
+            {totalRow.map((total) => {
+              return <tf key={total.id} className='bg-emerald-300'>{total}</tf>
             })}
+            <tf>{dailyTotal}</tf>
           </tr>
         </tfoot>
       </table>
     </>
-    // ) 
-    // :
-    // (
-    //   <h1>No Cookie Stands Created Yet</h1>
-    // )
+    ) 
+    :
+    (
+      <h1>No Cookie Stands Created Yet</h1>
+    )
   )
 }
 
